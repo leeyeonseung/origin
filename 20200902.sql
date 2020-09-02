7d7f9dcf677986b8d1d4304a276eeb35a6b589f1
yslee <mocuri8770@gmail.com>
2020-09-02 17:10:28 +0900

    "scott.sql, users.sql, basic_table.sql 준비
    SELECT 문법
    
    수업시간 코딩 rule
    
    NULL
    
    별칭
    
    리터럴, 리터럴 표기법
    
    문자열 연산, concat
    
    실습
    select1
    sel_con1"

---
+++ 20200902.sql
@@ -0,0 +1,65 @@
+SELECT * | {column | expression [alias] }
+FROM 테이블 이름;
+
+SELECT *
+FROM emp;
+
+SELECT *
+FROM dept;
+
+select empno, ename
+from emp;
+
+SELECT *
+FROM lprod;
+
+SELECT buyer_id, buyer_name
+FROM buyer;
+
+SELECT *
+FROM cart;
+
+SELECT mem_id, mem_pass, mem_name
+FROM member;
+
+SELECT ename, sal, sal+100
+FROM emp;
+
+DESC emp;
+
+SELECT *
+FROM emp;
+
+SELECT ename, hiredate, hiredate+365
+FROM emp;
+
+SELECT ename, hiredate, hiredate+365 after_1year, hiredate-365 before_1year
+FROM emp;
+
+SELECT *
+FROM dept;
+
+SELECT ename, sal, comm, sal + comm AS total_sal
+FROM emp;
+
+
+
+
+SELECT prod_id as id, prod_name as name
+FROM prod;
+
+SELECT lprod_gu as gu, lprod_nm as nm
+FROM lprod;
+
+SELECT buyer_id as "바이어아이디", buyer_name "이름"
+FROM buyer;
+
+SELECT ename||' ' || job
+FROM emp;
+
+SELECT CONCAT(CONCAT(ename,' '),job) 
+FROM emp;
+
+
+SELECT CONCAT('SELECT * FROM',CONCAT(' ',CONCAT(table_name, ';'))) as QUERY
+FROM user_tables;
