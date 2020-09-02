7d4da4d69c8a7b89d9274ee3fb782e7d6878d8b3
yslee <mocuri8770@gmail.com>
2020-09-02 17:08:31 +0900

    사용자 계정 생성

---
+++ 20200901_user_create.sql
@@ -0,0 +1,20 @@
+﻿--table space 생성
+SELECT *
+FROM DBA_DATA_FILES;
+
+CREATE TABLESPACE TS_DBSQL
+DATAFILE 'D:\B_UTIL\4.ORACLE\APP\ORACLE\ORADATA\XE\TS_DBSQL.DBF'
+SIZE 100M 
+AUTOEXTEND ON;
+
+
+--사용자 추가
+create user yslee identified by java
+default tablespace TS_DBSQL
+temporary tablespace temp
+quota unlimited on TS_DBSQL
+quota 0m on system;
+
+
+--접속, 생성권한
+GRANT CONNECT, RESOURCE TO yslee;
